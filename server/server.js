6946e6420dbf4a4a8f0e6be5ea86704aeeb6784c
Craig Clayton <craig__clayton@outlook.com>
2025-01-30 15:54:58 +0000

    comment by id working in postman

--- server/server.js
+++ server/server.js
@@ -31,5 +31,21 @@ app.get('/comments', async (req, res) => {
   }
 });
 
+app.get('/comments/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const { rows } = await db.query('SELECT * FROM comments WHERE id = $1', [
+      id,
+    ]);
+    if (rows.length === 0) {
+      return res.status(404).json({ message: 'Comment not found' });
+    }
+    res.status(200).json(rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
 const PORT = 8080;
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
