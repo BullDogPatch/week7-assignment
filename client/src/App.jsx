8c57059d8ab7f5ad94417d3c66b6ef81009ffaff
Craig Clayton <craig__clayton@outlook.com>
2025-01-31 09:37:27 +0000

    basic comments display to screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return <h1>Band Reviews</h1>;
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    const getComments = async () => {
+      const response = await fetch('http://localhost:8080/comments');
+      const data = await response.json();
+      console.log(data);
+      setComments(data);
+    };
+    getComments();
+  }, []);
+
+  return (
+    <>
+      <h1>Band Reviews</h1>
+      <ul>
+        {comments.map((comment) => (
+          <li key={comment.id}>{comment.description}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
