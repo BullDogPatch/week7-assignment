accf23661a4bee3b9df9f9cbd23b6ab2ad7533f8
Craig Clayton <craig__clayton@outlook.com>
2025-01-31 14:01:23 +0000

    PostForm works, made it so it is on a different route to comments but when you fill in form it redirects you to comments

---
+++ client/src/components/PostForm/PostForm.jsx
@@ -0,0 +1,93 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './PostForm.css';
+
+const PostForm = ({ setComments }) => {
+  const [formData, setFormData] = useState({
+    username: '',
+    band_name: '',
+    src: '',
+    description: '',
+    musical_rating: 1,
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch('http://localhost:8080/post-comment', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData),
+    });
+
+    const data = await response.json();
+
+    try {
+      if (response.ok) {
+        setComments((prev) => [data, ...prev]);
+        navigate('/comments');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <form className='form' onSubmit={handleSubmit}>
+      <label htmlFor='username'>Username:</label>
+      <input
+        value={formData.username}
+        type='text'
+        name='username'
+        id='username'
+        required
+        placeholder='Enter your name'
+        onChange={handleChange}
+      />
+
+      <label htmlFor='band-name'>Band Name:</label>
+      <input
+        value={formData.band_name}
+        type='text'
+        name='band_name'
+        id='band-name'
+        required
+        placeholder='Enter band here'
+        onChange={handleChange}
+      />
+
+      <label htmlFor='description'>Description:</label>
+      <textarea
+        value={formData.description}
+        name='description'
+        id='description'
+        rows='10'
+        placeholder='Please leave a comment'
+        required
+        onChange={handleChange}
+      ></textarea>
+
+      <label htmlFor='musical-rating'>Rating:</label>
+      <input
+        value={formData.musical_rating}
+        type='number'
+        name='musical_rating'
+        id='musical-rating'
+        required
+        onChange={handleChange}
+      />
+
+      <button type='submit'>Submit</button>
+    </form>
+  );
+};
+
+export default PostForm;
