c0f3cff1c72cd10a81d798e749dfc46b78339414
Craig Clayton <craig__clayton@outlook.com>
2025-02-02 21:18:07 +0000

    put the title in Link component that goes back to homepage from comments or users page

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
   return (
     <header className='header'>
-      <h1>BandReviews</h1>
+      <Link to='/' style={{ color: 'white' }}>
+        <h1>BandReviews</h1>
+      </Link>
       <div className='links'>
         <NavLink
           to='/comments'
