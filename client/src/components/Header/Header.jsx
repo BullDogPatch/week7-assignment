c37d5f8e191ea884855a2b8178d8b4bf61ff1d85
Craig Clayton <craig__clayton@outlook.com>
2025-02-02 12:18:44 +0000

    use react router NavLink in header to be able to have active path a different colour

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,9 +1,24 @@
+import { NavLink } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
   return (
     <header className='header'>
       <h1>BandReviews</h1>
+      <div className='links'>
+        <NavLink
+          to='/comments'
+          style={({ isActive }) => ({ color: isActive ? 'green' : 'blue' })}
+        >
+          Posts
+        </NavLink>
+        <NavLink
+          to='/users'
+          style={({ isActive }) => ({ color: isActive ? 'green' : 'blue' })}
+        >
+          Users
+        </NavLink>
+      </div>
     </header>
   );
 };
